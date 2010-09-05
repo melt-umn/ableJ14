3d397541cd53c9466084b107a1d44b210b09afdc
Lijesh Krishnan <lijesh@cs.umn.edu>
2010-09-05 19:52:31 +0000

    minor

--- composed/java_alone/Main.sv
+++ composed/java_alone/Main.sv
@@ -1,12 +1,6 @@
-grammar edu:umn:cs:melt:ableJ14:composed:java_alone; 
-
--- Collect the host language and desired language extensions
--- into one grammar by exporting them.
-exports edu:umn:cs:melt:ableJ14:host; 
-
--- Also, import the host language start nonterminal and
--- create a parser for the extended language.
-imports edu:umn:cs:melt:ableJ14:host only Root_C; 
+grammar edu:umn:cs:melt:ableJ14:composed:java_alone;
+exports edu:umn:cs:melt:ableJ14:host;
+imports edu:umn:cs:melt:ableJ14:host only Root_C;
 
 parser parse :: Root_C {
  edu:umn:cs:melt:ableJ14:composed:java_alone;
--- composed/java_alone/bin/Main.sv
+++ composed/java_alone/bin/Main.sv
@@ -1,9 +1,7 @@
-grammar edu:umn:cs:melt:ableJ14:composed:java_alone:bin; 
-
+grammar edu:umn:cs:melt:ableJ14:composed:java_alone:bin;
 import edu:umn:cs:melt:ableJ14:composed:java_alone;
 
 function main
-IO ::= args::String ioIn::IO
-{
-  return driver(args, ioIn, parse, parse) ;
+IO ::= args::String ioIn::IO {
+  return driver(args, ioIn, parse, parse);
 }
